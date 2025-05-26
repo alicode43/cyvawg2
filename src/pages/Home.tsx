2115cc88d48be0f4c8be2e12a6e604953a377e99
Ali <126307892+alicode43@users.noreply.github.com>
2025-05-26 12:25:01 +0530

    change csp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Home() {
  
@@ -199,11 +200,11 @@ export function Home() {
             practitioners, policymakers, and the wider public. */}
          The Case Studies feature six anonymised real-life accounts highlighting forms of cyber violence, including deepfakes, IoT abuse, and digital stalking. These narratives centre survivor experiences to inform and educate professionals, policymakers, and the public on emerging tech-facilitated harms.
           </p>
-          <a href="/case-stories" className="flex">
+          <Link to="/case-stories" className="flex">
             <button className="mt-4 bg-[#610064] text-white px-4 py-2 rounded hover:bg-purple-700 transition-colors shadow-md hover:scale-105 md:w-3/5 w-full md:absolute bottom-10 md:mx-[20%] mx-auto">
               Know more about Case Studies
             </button>
-          </a>
+          </Link>
         </div>
         {/* Card 2 */}
         <div className="md:w-[40%] md:p-[1%] p-10 rounded-[5%] h-full flex flex-col gap-10 shadow-lg transition-transform hover:scale-105 bg-white text-justify relative    ">
@@ -222,11 +223,11 @@ export function Home() {
             The CyVAWG Toolkit is a survivor-informed digital resource offering self-assessment tools, safety strategies, and legal guidance to help individuals identify and respond to tech-facilitated abuse. It also provides clear signposting to national and local support services 
             <br />
           </p>
-          <a href="/case-stories" className="flex">
+          <Link to="/case-stories" className="flex">
             <button className="mt-4 bg-[#610064] text-white px-4 py-2 rounded hover:bg-purple-700 transition-colors shadow-md hover:scale-105 md:w-3/5 w-full md:absolute bottom-10 md:mx-[20%] mx-auto ">
               Know more about CyVAWG Toolkit
             </button>
-          </a>
+          </Link>
         </div>
       </section>
 
